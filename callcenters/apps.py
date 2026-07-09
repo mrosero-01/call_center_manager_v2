60dcef2ee23239e5964e71aa0c708315573fda9e
Miguel Rosero <lenkev01@gmail.com>
2026-07-08 21:40:53 -0500

    inicializa proyecto de Django y se define el modelo de Client

---
+++ callcenters/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CallcentersConfig(AppConfig):
+    name = 'callcenters'
---
+++ clients/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
