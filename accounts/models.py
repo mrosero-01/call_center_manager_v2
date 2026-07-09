f6ab6195cd67d03b61eec7fd766329602a202cea
Miguel Rosero <lenkev01@gmail.com>
2026-07-08 23:09:16 -0500

    agrega usuario personalizado asociado a clientes

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import Q
 
-# Create your models here.
+
+class User(AbstractUser):
+    client = models.ForeignKey(
+        "clients.Client",
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                condition=(
+                    Q(is_superuser=True)
+                    | Q(client__isnull=False)
+                ),
+                name="user_superuser_or_has_client",
+            ),
+        ]
